385832cc9e21deed6e9b40a2b64f2da204c9a19a
anthonydanubio <anthonydanubio80@gmail.com>
2026-04-26 18:57:14 -0400

    Add Express server setup

---
+++ artifacts/api-server/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import pinoHttp from "pino-http";
+import router from "./routes";
+import { logger } from "./lib/logger";
+
+const app = express();
+
+app.use(pinoHttp({ logger }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+
+export default app;
